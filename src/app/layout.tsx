047fb04903d70ba9cd5f93209a98d3a72f7abbd6
Elton Souza <elton.souza1515@gmail.com>
2023-07-29 23:09:13 -0300

    chore: build initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SizeFashion",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
