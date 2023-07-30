fd948818d45ba265cdba4b2c83f0eb4342e2c9ac
Elton Souza <elton.souza1515@gmail.com>
2023-07-30 18:50:05 -0300

    feat: add clear search - export with memo

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,24 +1,37 @@
 "use client";
 import useFilter from "@/hooks/useFilter";
-import { ChangeEvent } from "react";
-import { FaSearch } from "react-icons/fa";
+import { ChangeEvent, memo } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
 
-export default function Search() {
-  const { setSearch } = useFilter();
+function Search() {
+  const { search, setSearch } = useFilter();
 
   const handleChangeTextSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const handleClearSearch = () => setSearch("");
+
   return (
-    <div className="flex items-center gap-2 px-2 py-1">
+    <div className="flex items-center gap-2 px-2 py-1 relative">
       <FaSearch />
-      <input
-        className="border-b-2 border-slate-400 focus-visible:outline-none focus-visible:border-black"
-        type="text"
-        placeholder="Search product"
-        onChange={handleChangeTextSearch}
-      />
+      <div className="relative">
+        <input
+          value={search}
+          className="border-b-2 border-slate-400 focus-visible:outline-none focus-visible:border-black w-[90%]"
+          type="text"
+          placeholder="Search product"
+          onChange={handleChangeTextSearch}
+        />
+        {search && (
+          <FaTimes
+            className="cursor-pointer absolute top-1 right-1"
+            onClick={handleClearSearch}
+          />
+        )}
+      </div>
     </div>
   );
 }
+
+export default memo(Search);
