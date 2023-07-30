591921d9aa14283c7ba06756bea07552b1f60bb0
Elton Souza <elton.souza1515@gmail.com>
2023-07-30 11:39:29 -0300

    feat: listing products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import useProducts from "@/hooks/useProducts";
+import ImageWrapper from "@/components/ImageWrapper";
+
 export default function Home() {
-  return <main></main>;
+  const { products } = useProducts();
+  return (
+    <main className="py-5 px-4 max-w-[1320px] mx-auto grid grid-cols-5 gap-3">
+      {products.map(({ image, title }) => {
+        return (
+          <div key={title} className="flex flex-col w-auto gap-3">
+            <ImageWrapper
+              src={image}
+              className="w-full h-[250px] object-contain"
+            />
+            <h3>{title}</h3>
+          </div>
+        );
+      })}
+    </main>
+  );
 }
