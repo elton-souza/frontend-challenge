a93b6da72fec0534b4a95b1e1335b0bd3bb923b0
Elton Souza <elton.souza1515@gmail.com>
2023-07-30 18:49:11 -0300

    refact: show category active - export with memo

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,29 +1,33 @@
 "use client";
 
+import { memo } from "react";
 import useFilter from "@/hooks/useFilter";
 import useProducts from "@/hooks/useProducts";
 import clsx from "clsx";
 
-export default function Categories() {
+function Categories() {
   const { filterType, setFilterType } = useFilter();
   const { categories } = useProducts();
 
   return (
     <ul className="flex items-center gap-4">
-      {categories.map((category) => (
-        <li
-          key={category}
-          className={clsx(
-            "font-normal tracking-widest uppercase cursor-pointer",
-            {
-              "font-bold": filterType === category,
-            }
-          )}
-          onClick={() => setFilterType(category)}
-        >
-          {category}
-        </li>
-      ))}
+      {categories.map((category) => {
+        const isActive = category === filterType;
+        return (
+          <li
+            key={category}
+            className={clsx("tracking-widest uppercase cursor-pointer", {
+              "font-normal": !isActive,
+              "font-bold": isActive,
+            })}
+            onClick={() => setFilterType(category)}
+          >
+            {category}
+          </li>
+        );
+      })}
     </ul>
   );
 }
+
+export default memo(Categories);
