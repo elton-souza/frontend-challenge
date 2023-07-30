bd17096fa8d038eef983d7e1538673f8a8bed417
Elton Souza <elton.souza1515@gmail.com>
2023-07-30 18:41:51 -0300

    refact: listing products - show only exists products - add not found text when search product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,34 @@ import ImageWrapper from "@/components/ImageWrapper";
 
 export default function Home() {
   const { products } = useProducts();
+
+  if (!products) return <></>;
+
   return (
-    <main className="py-5 px-4 max-w-[1320px] mx-auto grid grid-cols-5 gap-3">
-      {products.map(({ image, title }) => {
-        return (
-          <div key={title} className="flex flex-col w-auto gap-3">
-            <ImageWrapper
-              src={image}
-              className="w-full h-[250px] object-contain"
-            />
-            <h3>{title}</h3>
-          </div>
-        );
-      })}
+    <main className="py-5 px-4 min-h-screen max-w-[1320px] mx-auto">
+      {products.length ? (
+        <section className=" grid grid-cols-5 gap-10">
+          {products.map(({ image, title }) => {
+            return (
+              <div
+                key={title}
+                className="flex flex-col w-auto gap-3 cursor-pointer"
+              >
+                <ImageWrapper
+                  src={image}
+                  className="w-full h-[250px] object-contain"
+                />
+                <h3 className="text-base font-normal">{title}</h3>
+              </div>
+            );
+          })}
+        </section>
+      ) : (
+        <div className="flex flex-col justify-center items-center mt-10">
+          <h3 className="text-2xl mb-2">No products found!</h3>
+          <span className="text-xl">Clear your search and try again.</span>
+        </div>
+      )}
     </main>
   );
 }
